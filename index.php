871fbde6289826d4d9c7eeddb7ee764684d63fd9
dembasy <dembasy1227@gmail.com>
2018-07-30 09:44:06 +0200

    done

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$ten = 0;
+ ?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>ex1</title>
+  </head>
+  <body>
+<?php
+while ($ten < 10) {
+  echo $ten;
+  $ten++;
+}
+ ?>
+  </body>
+</html>
